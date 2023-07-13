ee1a3c6bb1042d12b6fb57d28cab6d2a3cf55d5b
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-07-13 21:35:52 +0200

    App finished, it was pretty much usefull coding. Especially conditional actions and setting up id. :)

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ function FlashCards() {
   const [selectedId, setSelectedId] = useState(null);
 
   function handleClick(id) {
-    setSelectedId(id);
+    setSelectedId(id !== selectedId ? id : null);
   }
 
   return (
