da9cc94e7c5205cb1321f34c0c1df779710044e5
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-07-13 20:17:38 +0200

    Mapping questions, and rendering questions.

--- src/App.js
+++ src/App.js
@@ -12,36 +12,44 @@ const questions = [
   {
     id: 3457,
     question: "What language is React based on?",
-    answer: "JavaScript"
+    answer: "JavaScript",
   },
   {
     id: 7336,
     question: "What are the building blocks of React apps?",
-    answer: "Components"
+    answer: "Components",
   },
   {
     id: 8832,
     question: "What's the name of the syntax we use to describe a UI in React?",
-    answer: "JSX"
+    answer: "JSX",
   },
   {
     id: 1297,
     question: "How to pass data from parent to child components?",
-    answer: "Props"
+    answer: "Props",
   },
   {
     id: 9103,
     question: "How to give components memory?",
-    answer: "useState hook"
+    answer: "useState hook",
   },
   {
     id: 2002,
     question:
       "What do we call an input element that is completely synchronised with state?",
-    answer: "Controlled element"
-  }
+    answer: "Controlled element",
+  },
 ];
 
 function FlashCards() {
-  return <div>TODO</div>;
+  return (
+    <div className="flashcards">
+      {questions.map((question) => (
+        <div>
+          <p> {question.question} </p>
+        </div>
+      ))}
+    </div>
+  );
 }
